d366a6ae2c9263471f3896e48fed27b48fc4798f
Ebalin1411 <ebalinrosecse2006@gmail.com>
2025-01-03 14:21:22 +0530

    feat:getting base url from env file

--- src/app/service/store.service.ts
+++ src/app/service/store.service.ts
@@ -2,24 +2,21 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Store } from '../models/storemodel';
 import { Observable } from 'rxjs';
+import { environment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StoreService {
   constructor(private httpClient: HttpClient) {}
-  baseUrl = 'http://localhost:5013/api/Store';
+  baseUrl = environment.apiBaseUrl;
   GetStores(): Observable<Store[]> {
-    return this.httpClient.get<Store[]>(this.baseUrl);
+    return this.httpClient.get<Store[]>(`${this.baseUrl}`);
   }
 
   CreateStoreInfo(storeInfo: Store): Observable<Store> {
     storeInfo.Id = '00000000-0000-0000-0000-000000000000';
     console.log('inside Create store info');
-    return this.httpClient.post<Store>(this.baseUrl, storeInfo, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-      }),
-    });
+    return this.httpClient.post<Store>(`${this.baseUrl}`, storeInfo);
   }
 }
